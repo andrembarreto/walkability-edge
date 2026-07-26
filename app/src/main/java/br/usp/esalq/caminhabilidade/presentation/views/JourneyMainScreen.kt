06faaa996770c5a81015781edcb88a9ca5ca73da
Andre Barreto <andremoratob@gmail.com>
2026-07-26 14:16:12 -0300

    feat: structured journey main screen

--- app/src/main/java/br/usp/esalq/caminhabilidade/presentation/views/JourneyMainScreen.kt
+++ app/src/main/java/br/usp/esalq/caminhabilidade/presentation/views/JourneyMainScreen.kt
@@ -1,8 +1,112 @@
 package br.usp.esalq.caminhabilidade.presentation.views
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.QuestionMark
+import androidx.compose.material.icons.filled.Shield
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.wear.compose.material3.Icon
+import androidx.wear.compose.material3.IconButton
+import androidx.wear.compose.material3.MaterialTheme
+import androidx.wear.compose.material3.ScreenScaffold
+import androidx.wear.compose.material3.Text
+import androidx.wear.compose.ui.tooling.preview.WearPreviewDevices
+import androidx.wear.compose.ui.tooling.preview.WearPreviewFontScales
 
 @Composable
 fun JourneyMainScreen() {
+    ScreenScaffold() { contentPadding ->
+        Column(
+            modifier = Modifier
+                .padding(contentPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            HelpButton()
+            DimensionSelector()
+            StatusBar()
+        }
+    }
+}
 
+@Composable
+private fun HelpButton() {
+    IconButton(
+        onClick = { /* TODO */ },
+        modifier = Modifier
+            .background(color = MaterialTheme.colorScheme.secondaryDim, shape = CircleShape)
+            .padding(4.dp)
+            .size(12.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Default.QuestionMark,
+            contentDescription = "Ajuda"
+        )
+    }
+}
+
+@Composable
+private fun DimensionSelector() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Segurança"
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Icon(
+            imageVector = Icons.Default.Shield,
+            contentDescription = "Segurança",
+            modifier = Modifier.size(30.dp)
+        )
+    }
+}
+
+@Composable
+private fun StatusBar() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Em percurso",
+                fontSize = 12.sp
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Icon(
+                imageVector = Icons.Default.Pause,
+                contentDescription = "Pausar",
+                modifier = Modifier.size(16.dp)
+            )
+        }
+        Text(
+            text = "10:20:30",
+            fontSize = 10.sp
+        )
+    }
+}
+
+@WearPreviewDevices
+@WearPreviewFontScales
+@Composable
+fun DefaultPreview() {
+    JourneyMainScreen()
 }
