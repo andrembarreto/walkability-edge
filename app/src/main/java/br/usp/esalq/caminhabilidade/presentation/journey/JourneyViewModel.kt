f513b73b40adf831f46c5336bde994cf2664b822
Andre Barreto <andremoratob@gmail.com>
2026-07-28 20:55:00 -0300

    feat: (actually) added journey view model contract

--- app/src/main/java/br/usp/esalq/caminhabilidade/presentation/journey/JourneyViewModel.kt
+++ app/src/main/java/br/usp/esalq/caminhabilidade/presentation/journey/JourneyViewModel.kt
@@ -1,4 +1,17 @@
 package br.usp.esalq.caminhabilidade.presentation.journey
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+
 class JourneyViewModel {
+
+    val elapsedTime by mutableStateOf("00:00:00")
+
+    fun startJourney() {
+
+    }
+
+    fun stopJourney() {
+
+    }
 }
