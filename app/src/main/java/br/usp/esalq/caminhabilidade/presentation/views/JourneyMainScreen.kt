30676efec31637ec3f1399a6eaa5a1cf978cecd8
Andre Barreto <andremoratob@gmail.com>
2026-07-28 18:58:04 -0300

    feat: added dialog to confirm journey finishing

--- app/src/main/java/br/usp/esalq/caminhabilidade/presentation/views/JourneyMainScreen.kt
+++ app/src/main/java/br/usp/esalq/caminhabilidade/presentation/views/JourneyMainScreen.kt
@@ -12,14 +12,22 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.QuestionMark
+import androidx.compose.material.icons.filled.RemoveRoad
 import androidx.compose.material.icons.filled.Shield
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.wear.compose.material3.AlertDialog
 import androidx.wear.compose.material3.Icon
 import androidx.wear.compose.material3.IconButton
 import androidx.wear.compose.material3.MaterialTheme
@@ -80,6 +88,8 @@ private fun DimensionSelector() {
 
 @Composable
 private fun StatusBar() {
+    var showFinishDialog by remember { mutableStateOf(false) }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -91,22 +101,55 @@ private fun StatusBar() {
                 fontSize = 12.sp
             )
             Spacer(modifier = Modifier.width(4.dp))
-            Icon(
-                imageVector = Icons.Default.Pause,
-                contentDescription = "Pausar",
+            IconButton(
+                onClick = { showFinishDialog = true },
                 modifier = Modifier.size(16.dp)
-            )
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Pause,
+                    contentDescription = "Pausar",
+                )
+            }
         }
         Text(
             text = "10:20:30",
             fontSize = 10.sp
         )
     }
+
+    AlertDialog(
+        visible = showFinishDialog,
+        onDismissRequest = { showFinishDialog = false },
+        title = { Text("") },
+        text = { Text("Finalizar percurso?") },
+        icon = {
+            Icon(
+                imageVector = Icons.Default.RemoveRoad,
+                contentDescription = "Estrada X"
+            )
+        },
+        dismissButton = {
+            IconButton(onClick = { showFinishDialog = false }) {
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    contentDescription = ""
+                )
+            }
+        },
+        confirmButton = {
+            IconButton(onClick = { /* TODO */ }) {
+                Icon(
+                    imageVector = Icons.Default.Check,
+                    contentDescription = ""
+                )
+            }
+        },
+    )
 }
 
 @WearPreviewDevices
 @WearPreviewFontScales
 @Composable
-fun DefaultPreview() {
+fun JourneyPreview() {
     JourneyMainScreen()
 }
