054d98a7e87855e688b73b9075ac93785ce2b741
Andre Barreto <andremoratob@gmail.com>
2026-07-28 21:02:27 -0300

    feat: setup routes on wear app navigation

--- app/src/main/java/br/usp/esalq/caminhabilidade/presentation/navigation/WearAppNavigation.kt
+++ app/src/main/java/br/usp/esalq/caminhabilidade/presentation/navigation/WearAppNavigation.kt
@@ -1,27 +1,47 @@
 package br.usp.esalq.caminhabilidade.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.wear.compose.material3.ScreenScaffold
 import androidx.wear.compose.navigation.SwipeDismissableNavHost
 import androidx.wear.compose.navigation.composable
 import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
+import br.usp.esalq.caminhabilidade.presentation.home.HomeRoute
 import br.usp.esalq.caminhabilidade.presentation.home.HomeScreen
+import br.usp.esalq.caminhabilidade.presentation.journey.JourneyMainRoute
 import br.usp.esalq.caminhabilidade.presentation.journey.JourneyMainScreen
+import br.usp.esalq.caminhabilidade.presentation.journey.JourneyViewModel
 
 @Composable
 fun WearAppNavigation() {
     val navController = rememberSwipeDismissableNavController()
+    val journeyViewModel = remember {
+        JourneyViewModel()
+    }
 
     SwipeDismissableNavHost(
         navController = navController,
         startDestination = "home"
     ) {
         composable("home") {
-            HomeScreen(
-                onStart = { navController.navigate("journeyMain") }
+            HomeRoute(
+                viewModel = journeyViewModel,
+                onNavigateToJourney = {
+                    navController.navigate("journeyMain")
+                }
             )
         }
         composable("journeyMain") {
-            JourneyMainScreen()
+            JourneyMainRoute(
+                viewModel = journeyViewModel,
+                onNavigateToFinish = {
+                    navController.navigate("journeyFinish")
+                }
+            )
+        }
+        composable("journeyFinish") {
+            ScreenScaffold {}
         }
     }
 }
